a603324925e224db7428419450c91cd9de9af54b
Sisi Hu <sisi.hu@headspace.com>
2022-05-06 10:00:42 -1000

    project completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 import CreateArea from "./CreateArea.jsx";
 
 function App() {
-    return <div>
-        <Header />
-        <CreateArea />
-        <Note key={1} title="Note title" content="Note Content" />
-        <Footer />
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
